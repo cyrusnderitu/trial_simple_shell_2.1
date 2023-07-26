f1e0d0ff27baf6cf24d773a45d037c6d43f12776
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-07-26 19:02:47 +0300

    chat gpt code example for simple shell

--- shell.c
+++ shell.c
@@ -1,39 +1,90 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/**
- * main - entry point to compiling our shell
- * @argc:  argument count
- * @argv: argument vector
- *Return: 0 when successful, -1 otherwise
- */
-int main(int argc, char *argv[]__attribute__((unused)))
-{
-	char *testr, *line = NULL;
-	size_t buff;
-	int nget;
+#define MAX_CMD_LENGTH 1024
+#define MAX_ARGS 64
 
-	if (isatty(STDIN_FILENO) == 0 && argc == 1)
-	{
-		while ((nget = getline(&line, &buff, stdin)) > 0)
-		{
-			testr = malloc(sizeof(char) * nget + 1);
-			_strcpy(testr, line);
-			finalizer(testr);
-			wait(NULL);
-		}
-		free(line);
-		return (0);
-	}
-	do {
-		testr = collect_args();
-		if (testr[0] == '\0' || _strcmp(testr, "\n") == 0)
-		{
-			free(testr);
-			continue;
-		}
-		finalizer(testr);
-		wait(NULL);
-		free(testr);
-	} while (1);
-	return (0);
+void execute_command(char* args[]) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("Command execution failed");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("Fork error");
+    } else {
+        wait(NULL);
+    }
+}
+
+void parse_input(char* input, char* args[]) {
+    int arg_index = 0;
+    char* token = strtok(input, " \t\n");
+    while (token != NULL) {
+        args[arg_index++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    args[arg_index] = NULL;
+}
+
+int main() {
+    char input[MAX_CMD_LENGTH];
+    char* args[MAX_ARGS];
+    int background = 0;
+
+    while (1) {
+        printf("shell$ ");
+        fflush(stdout);
+
+        // Read user input
+        if (fgets(input, MAX_CMD_LENGTH, stdin) == NULL) {
+            break;
+        }
+
+        // Check for background process (ends with &)
+        background = 0;
+        int input_len = strlen(input);
+        if (input_len > 1 && input[input_len - 2] == '&') {
+            background = 1;
+            input[input_len - 2] = '\n'; // Remove the '&'
+        }
+
+        // Parse input into arguments
+        parse_input(input, args);
+
+        // Handle built-in commands
+        if (args[0] != NULL) {
+            if (strcmp(args[0], "exit") == 0) {
+                break;
+            } else if (strcmp(args[0], "cd") == 0) {
+                if (args[1] != NULL) {
+                    if (chdir(args[1]) == -1) {
+                        perror("cd");
+                    }
+                }
+                continue;
+            }
+        }
+
+        // Execute command
+        if (args[0] != NULL) {
+            if (background) {
+                // Fork and execute in the background
+                pid_t bg_pid = fork();
+                if (bg_pid == 0) {
+                    execute_command(args);
+                    exit(EXIT_SUCCESS);
+                }
+            } else {
+                execute_command(args);
+            }
+        }
+    }
+
+    return 0;
 }
